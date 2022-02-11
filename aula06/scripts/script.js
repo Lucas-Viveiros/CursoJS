19f045c00d80a9ae601dfc149a03399cecb2e658
Lucas Viveiros <98289153+Lucas-Viveiros@users.noreply.github.com>
2022-02-11 02:02:22 -0300

    Alguns exercícios criados em JavaScript

---
+++ aula06/scripts/script.js
@@ -0,0 +1,2 @@
+var nome = window.prompt('Qual seu nome?')
+window.alert('É um grande prazer te conhecer, ' + nome + '!') // Concatenação
---
+++ aula06/scripts/script2.js
@@ -0,0 +1,12 @@
+
+var n1 = Number(window.prompt('Digite um número: '))
+var n2 = Number(window.prompt('Digite outro número: ')) 
+var s = n1 + n2
+window.alert(`A soma entre ${n1} e ${n2} resulta em ${s}!`)
+// (number + number) para adição
+// (string + string) para concatenação
+
+/*
+var s = window.prompt('O que você está aprendendo?')
+window.alert('Você está gostando de aprender ' + s + '?')
+*/
