ede32758a6c38dded9722988945e697a7c0f66b2
Lucas Viveiros <98289153+Lucas-Viveiros@users.noreply.github.com>
2022-02-11 06:33:56 -0300

    exercício de idade criado

---
+++ aula06/scripts/script4.js
@@ -0,0 +1,5 @@
+var ano = parseInt(window.prompt('Em que ano estamos?'))
+var nasc = parseInt(window.prompt('Em que ano você nasceu?'))
+var idade = ano - nasc
+
+window.alert(`Ok! Com essas informações, eu posso afirmar que você tem ${idade} anos, correto?`)
