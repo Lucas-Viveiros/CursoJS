6aed4b5bb443dd789b171510464f66274aef1899
Lucas Viveiros <98289153+Lucas-Viveiros@users.noreply.github.com>
2022-02-12 11:05:00 -0300

    condições criadas

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 65
+console.log(`A velocidade do seu carro é de ${vel} km/h.`)
+if (vel > 60) { // Condição Simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log('Dirija sempre usando cinto de segurança!')
+
