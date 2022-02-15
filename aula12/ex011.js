865d3c26b1d65c3a7992a979468bc2e5f58ac89d
Lucas Viveiros <98289153+Lucas-Viveiros@users.noreply.github.com>
2022-02-15 13:03:22 -0300

    exercícios e desafios em desenvolvimento

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 18
+if (idade >= 18 && idade < 60) {
+    console.log ('Você pode beber álcool!')
+}   else if (idade < 16) {
+    console.log ('Você não pode beber álcool!')
+}   else if (idade < 18 || idade > 60) {
+    console.log ('Você pode beber álcool, mas não é recomendável!')
+
+}
