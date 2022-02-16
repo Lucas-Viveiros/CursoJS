b47574c290f6f94320161d613ef690f447243773
Lucas Viveiros <98289153+Lucas-Viveiros@users.noreply.github.com>
2022-02-16 15:18:07 -0300

    Site interativo com JavaScript criado

---
+++ aula12ex/ex014/scripts/script.js
@@ -0,0 +1,26 @@
+function carregar () {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+
+    msg.innerHTML = `Agora são ${hora} horas e ${minuto} minutos!`
+
+    if (hora >= 0 && hora < 6) {
+        img.src = 'imagens/foto-madrugada.png'
+        document.body.style.background = '#ffb102'
+    } else if (hora >= 6 && hora < 12) {
+        // BOM DIA!
+        img.src = 'imagens/foto-manha.png'
+        document.body.style.background = '#fbc084'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE!
+        img.src = 'imagens/foto-tarde.png'
+        document.body.style.background = '#8c3b28'
+    } else {
+        // BOA NOITE!
+        img.src = 'imagens/foto-noite.png'
+        document.body.style.background = '#568c97'
+    }
+}
